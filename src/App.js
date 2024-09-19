69055a418baae521fbba0aadc6f00dc315d2e2fb
Vitalya <vitalikt_22@mail.ru>
2024-09-19 14:34:35 +0400

    adding get,put, delete functional. Working on adding patch

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className='App'>
       <header className='App-header'>
+        <div className='main-title'>Todo</div>
         <Todo />
       </header>
     </div>
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,26 +1,111 @@
 import { useEffect, useState } from 'react'
 import styles from '../css/todo.module.css'
+import TodoInput from './TodoInput'
 
 const Todo = () => {
   const [todos, setTodos] = useState([])
+  const [inputCreateValue, setInputCreateValue] = useState([])
+  const [inputUpdateValue, setInputUpdateValue] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [isCreating, setIsCreating] = useState(false)
+  const [isUpdating, setIsUpdating] = useState(false)
+  // const [isChanging, setIsChanging] = useState(false)
+  const [isDeleting, setIsDeleting] = useState(false)
+  const [refreshTodosFlag, setRefreshTodosFlag] = useState(false)
+  // const [isDone, setIsDone] = useState(false)
+
+  const refreshTodos = () => setRefreshTodosFlag(!refreshTodosFlag)
 
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/todos')
+    setIsLoading(true)
+
+    fetch('http://localhost:3005/todos')
       .then((loadedData) => loadedData.json())
       .then((loadedTodos) => {
         setTodos(loadedTodos)
       })
-  }, [])
-  console.log(todos)
+      .finally(() => setIsLoading(false))
+  }, [refreshTodosFlag])
+
+  const updateTodo = (id) => {
+    setIsUpdating(true)
+
+    fetch(`http://localhost:3005/todos/${id}`, {
+      method: 'PATCH',
+      headers: { 'Content-Type': 'application/json;charset=utf-8' },
+      body: JSON.stringify({
+        title: inputUpdateValue,
+        completed: false ? true : false,
+      }),
+    })
+      .then((rawResponse) => rawResponse.json())
+      .then((response) => {
+        console.log('Дело изменено!', response)
+
+        refreshTodos()
+      })
+      .finally(() => setIsUpdating(false))
+  }
+
+  const deleteTodo = (id) => {
+    setIsDeleting(true)
+
+    fetch(`http://localhost:3005/todos/${id}`, {
+      method: 'DELETE',
+    })
+      .then((rawResponse) => rawResponse.json())
+      .then((response) => {
+        console.log('Дело удалено!', response)
+
+        refreshTodos()
+      })
+      .finally(() => setIsDeleting(false))
+  }
 
   return (
     <>
-      <div className={styles['main-title']}>Todo</div>
-      {todos.map(({ id, title }) => (
-        <div className={styles.todo} key={id}>
-          {title}
-        </div>
-      ))}
+      <TodoInput
+        setIsCreating={setIsCreating}
+        isCreating={isCreating}
+        refreshTodos={refreshTodos}
+        setInputCreateValue={setInputCreateValue}
+        inputCreateValue={inputCreateValue}
+      />
+      {isLoading ? (
+        <div className={styles.loader}></div>
+      ) : (
+        todos.map(({ id, title }) => (
+          <div className={styles.todo} key={id}>
+            {!1 ? (
+              <form
+                onSubmit={(e) => {
+                  e.preventDefault()
+                  updateTodo(id)
+                }}
+              >
+                <input
+                  value={inputUpdateValue}
+                  onChange={({ target }) => {
+                    setInputUpdateValue(target.value)
+                  }}
+                ></input>
+              </form>
+            ) : (
+              title
+            )}
+            <div>
+              {!1 ? (
+                <button type='submit' onClick={() => console.log(id)}>
+                  Применить
+                </button>
+              ) : (
+                <button onClick={() => !1}>Изменить</button>
+              )}
+              <button onClick={() => deleteTodo(id)}>Удалить</button>
+            </div>
+          </div>
+        ))
+      )}
     </>
   )
 }
